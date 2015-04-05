972b054a4bf0c1b2273fedf03d2b27c75116cf0e
raym <a4265622@opayq.com>
2015-04-05 10:53:00 -0700

    create first helper. use minimongo

--- em/01_getting-started-with-meteor/04_build-a-simple-app/README.md
+++ em/01_getting-started-with-meteor/04_build-a-simple-app/README.md
@@ -4,3 +4,8 @@ todos $ meteor add bootstrap
 todos $ meteor add less
 todos $ mv todos.css todos.less
 ```
+```js
+Todos.findOne().is_done
+// check checkbox
+Todos.findOne().is_done
+```
--- em/01_getting-started-with-meteor/04_build-a-simple-app/todos/todos.js
+++ em/01_getting-started-with-meteor/04_build-a-simple-app/todos/todos.js
@@ -12,6 +12,20 @@ if (Meteor.isClient) {
       return this.is_done ? 'checked' : '';
     }
   });
+
+  Template.TodoItem.events({
+    'click [name=is_done]': function(e, tmpl) {
+      var
+        id = this._id,
+        isDone = tmpl.find('input').checked
+      ;
+      Todos.update({_id: id}, {
+        $set: {
+          is_done: isDone
+        }
+      });
+    }
+  });
 }
 
 if (Meteor.isServer) {
