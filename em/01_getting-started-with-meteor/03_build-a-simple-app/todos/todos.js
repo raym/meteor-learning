f1803911aed281cb984c23005883ab81600c3d96
raym <a4265622@opayq.com>
2015-04-04 21:19:47 -0700

    use block expression (pound symbol) to render the list items with a template in a template. create helper for populating list in template. observe data context in different templates

--- em/01_getting-started-with-meteor/03_build-a-simple-app/todos/todos.js
+++ em/01_getting-started-with-meteor/03_build-a-simple-app/todos/todos.js
@@ -1,6 +1,11 @@
 Todos = new Meteor.Collection('todos');
 
 if (Meteor.isClient) {
+  Template.TodosPanel.helpers({
+    items: function() {
+      return Todos.find();
+    }
+  });
 }
 
 if (Meteor.isServer) {
