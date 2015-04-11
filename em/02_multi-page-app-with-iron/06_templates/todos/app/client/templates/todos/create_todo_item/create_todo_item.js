c299f5ddf5427b4a830206c4643e6df4665dc64f
raym <a4265622@opayq.com>
2015-04-11 14:56:33 -0700

    create todo item template with form submit handler

--- em/02_multi-page-app-with-iron/06_templates/todos/app/client/templates/todos/create_todo_item/create_todo_item.js
+++ em/02_multi-page-app-with-iron/06_templates/todos/app/client/templates/todos/create_todo_item/create_todo_item.js
@@ -2,6 +2,19 @@
 /* CreateTodoItem: Event Handlers */
 /*****************************************************************************/
 Template.CreateTodoItem.events({
+  'submit form': function(e, tmpl) {
+    e.preventDefault();
+    var subject = tmpl.find('input').value;
+
+    Todos.insert({
+      subject: subject,
+      createdAt: new Date,
+      isDone: false,
+      userId: Meteor.userId()
+    });
+
+    tmpl.find('form').reset();
+  }
 });
 
 /*****************************************************************************/
