eec0a31cb8f5f16082edd1ccb57b9060206d4d77
raym <a4265622@opayq.com>
2015-04-11 13:02:34 -0700

    fill out `todos_list` template and generate nested templates for `todos_list`

---
+++ em/02_multi-page-app-with-iron/06_templates/todos/app/client/templates/todos/create_todo_item/create_todo_item.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* CreateTodoItem: Event Handlers */
+/*****************************************************************************/
+Template.CreateTodoItem.events({
+});
+
+/*****************************************************************************/
+/* CreateTodoItem: Helpers */
+/*****************************************************************************/
+Template.CreateTodoItem.helpers({
+});
+
+/*****************************************************************************/
+/* CreateTodoItem: Lifecycle Hooks */
+/*****************************************************************************/
+Template.CreateTodoItem.created = function () {
+};
+
+Template.CreateTodoItem.rendered = function () {
+};
+
+Template.CreateTodoItem.destroyed = function () {
+};
