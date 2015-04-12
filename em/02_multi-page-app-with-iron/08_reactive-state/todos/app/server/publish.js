404f76383bb910979a4b00df781d16eefb629fd6
raym <a4265622@opayq.com>
2015-04-12 14:03:29 -0700

    inital state of 08

---
+++ em/02_multi-page-app-with-iron/08_reactive-state/todos/app/server/publish.js
@@ -0,0 +1,15 @@
+/**
+ * Meteor.publish('items', function (param1, param2) {
+ *  this.ready();
+ * });
+ */
+
+
+Meteor.publish('todos', function (/* args */) {
+  return Todos.find({userId: this.userId});
+});
+
+
+Meteor.publish('todo', function (id) {
+  return Todos.find({_id: id});
+});
