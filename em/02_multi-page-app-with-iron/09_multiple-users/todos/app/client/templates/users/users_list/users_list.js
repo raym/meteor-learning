a342eb25de263b91c2981edb80923c8de1023b1d
raym <a4265622@opayq.com>
2015-04-15 08:42:21 -0700

    hide the logged in user from the users list. let the user know if they're the only user in the system

--- em/02_multi-page-app-with-iron/09_multiple-users/todos/app/client/templates/users/users_list/users_list.js
+++ em/02_multi-page-app-with-iron/09_multiple-users/todos/app/client/templates/users/users_list/users_list.js
@@ -9,7 +9,7 @@ Template.UsersList.events({
 /*****************************************************************************/
 Template.UsersList.helpers({
   users: function() {
-    return Meteor.users.find(); //{_id: {$ne: Meteor.userId()}});
+    return Meteor.users.find({_id: {$ne: Meteor.userId()}});
   }
 });
 
