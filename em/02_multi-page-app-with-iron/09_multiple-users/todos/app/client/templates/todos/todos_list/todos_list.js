23932d73547c8cf81a09984f58b043912eb8fa59
raym <a4265622@opayq.com>
2015-04-14 21:39:44 -0700

    initial state of 09

---
+++ em/02_multi-page-app-with-iron/09_multiple-users/todos/app/client/templates/todos/todos_list/todos_list.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* TodosList: Event Handlers */
+/*****************************************************************************/
+Template.TodosList.events({
+});
+
+/*****************************************************************************/
+/* TodosList: Helpers */
+/*****************************************************************************/
+Template.TodosList.helpers({
+  todos: function() {
+    return Todos.find({}, {
+      sort: { createdAt: -1 }
+    });
+  },
+
+  isDoneClass: function() {
+    return this.isDone ? 'done' : '';
+  }
+});
+
+/*****************************************************************************/
+/* TodosList: Lifecycle Hooks */
+/*****************************************************************************/
+Template.TodosList.created = function () {
+};
+
+Template.TodosList.rendered = function () {
+};
+
+Template.TodosList.destroyed = function () {
+};
