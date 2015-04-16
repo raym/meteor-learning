50b06f1633774a74cfb874716f04b8d9575d9052
raym <a4265622@opayq.com>
2015-04-16 08:21:44 -0700

    provide event handlers and helpers for comments list. use momentjs to show the time in a human readable way

--- em/02_multi-page-app-with-iron/10_comments/README.md
+++ em/02_multi-page-app-with-iron/10_comments/README.md
@@ -2,4 +2,5 @@
 10 $ cd todos
 todos $ iron g:collection comments
 todos $ iron g:template todos/comments_list
+todos $ iron add momentjs:moment
 ```
--- em/02_multi-page-app-with-iron/10_comments/todos/app/client/templates/todos/comments_list/comments_list.js
+++ em/02_multi-page-app-with-iron/10_comments/todos/app/client/templates/todos/comments_list/comments_list.js
@@ -2,12 +2,44 @@
 /* CommentsList: Event Handlers */
 /*****************************************************************************/
 Template.CommentsList.events({
+  'submit form#new-comment': function(e, tmpl) {
+    e.preventDefault();
+
+    var todo = this;
+    var body = tmpl.find('textarea[name=body]').value;
+    var form = tmpl.find('form');
+
+    Comments.insert({
+      todoId: todo._id,
+      createdAt: new Date,
+      userId: Meteor.userId(),
+      body: body
+    });
+
+    form.reset();
+  }
 });
 
 /*****************************************************************************/
 /* CommentsList: Helpers */
 /*****************************************************************************/
 Template.CommentsList.helpers({
+  comments: function() {
+    var todo = this;
+    return Comments.find({todoId: todo._id}, {
+      sort: {createdAt: -1}
+    });
+  },
+
+  user: function() {
+    var comment = this;
+    return Meteor.users.findOne({_id: comment.userId});
+  },
+
+  timestamp: function() {
+    var comment = this;
+    return moment(comment.createdAt).fromNow();
+  }
 });
 
 /*****************************************************************************/
